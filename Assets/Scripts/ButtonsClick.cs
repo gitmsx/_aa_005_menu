5aab35927fe27b795828412d6388be0198dc000f
gitmsx <gitmsx@mail.ru>
2022-12-11 13:30:48 +0300

    Saved on 11.12.2022-13:30:47,33

--- Assets/Scripts/ButtonsClick.cs
+++ Assets/Scripts/ButtonsClick.cs
@@ -1,19 +1,24 @@
 
+using System.Data.SqlTypes;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class ButtonsClick : MonoBehaviour
 {
     //Make sure to attach these Buttons in the Inspector
-    public Button m_YourFirstButton, m_YourSecondButton, m_YourThirdButton;
+    public Button Button0, Button1, Button2, Button3;
+    [SerializeField] TextMeshProUGUI TextButton0, TextButton1, TextButton2, TextButton3;
 
     void Start()
     {
         //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
-        m_YourFirstButton.onClick.AddListener(TaskOnClick);
-        m_YourSecondButton.onClick.AddListener(delegate { TaskWithParameters("Hello"); });
-        m_YourThirdButton.onClick.AddListener(() => ButtonClicked(42));
-        m_YourThirdButton.onClick.AddListener(TaskOnClick);
+        Button0.onClick.AddListener(TaskOnClick);
+        Button1.onClick.AddListener(TaskOnClick);
+        Button2.onClick.AddListener(delegate { TaskWithParameters("Hello"); });
+        Button3.onClick.AddListener(() => ButtonClicked(42));
+
     }
 
 
@@ -25,9 +30,58 @@ public class ButtonsClick : MonoBehaviour
     void TaskOnClick()
     {
         //Output this to console when Button1 or Button3 is clicked
-        Debug.Log("You have clicked the button!");
-    }
+     // Debug.Log("You have clicked the button!");
 
+        string[] stringsLevel = LevelLoad.ReadData(1, 1);
+
+        
+        string[] buttonsTextNew = new string[12];
+        buttonsTextNew[0] = "";
+        buttonsTextNew[1] = "";
+        buttonsTextNew[2] = "";
+        buttonsTextNew[3] = "";
+
+        string[] buttonsSubStrSearch = new string[12];
+        buttonsSubStrSearch[0] = "###1:";
+        buttonsSubStrSearch[1] = "###2:";
+        buttonsSubStrSearch[2] = "###3:";
+        buttonsSubStrSearch[3] = "###4:";
+
+
+        Button[] buttonsGO = new Button[12];
+        buttonsGO[0] = Button0;
+        buttonsGO[1] = Button1;
+        buttonsGO[2] = Button2;
+        buttonsGO[3] = Button3;
+
+
+        TextMeshProUGUI[] TextBbuttonsGO = new TextMeshProUGUI[12];
+        TextBbuttonsGO[0] = TextButton0;
+        TextBbuttonsGO[1] = TextButton1;
+        TextBbuttonsGO[2] = TextButton2;
+        TextBbuttonsGO[3] = TextButton3;
+
+
+
+
+
+
+        string substring ;
+        print("stringsLevel.Length " + stringsLevel.Length.ToString());
+        for (int i = 0; i < stringsLevel.Length; i++)
+        {
+      //      print(stringsLevel[i]);
+            for (int j = 0; j < 4; j++)
+            {
+                
+                if (stringsLevel[i].IndexOf(buttonsSubStrSearch[j]) > 0)
+                {
+                    TextBbuttonsGO[j].text = stringsLevel[i];
+                    print(stringsLevel[i]);
+                }
+            }
+        }
+    }
     void TaskWithParameters(string message)
     {
         //Output this to console when the Button2 is clicked
@@ -40,3 +94,6 @@ public class ButtonsClick : MonoBehaviour
         Debug.Log("Button clicked = " + buttonNo);
     }
 }
+
+
+
